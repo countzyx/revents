72fa78a2671732dc07b88ac04b815dbea3363aa8
Pedro R. Jiménez <countzyx@gmail.com>
2021-02-20 03:21:03 -0500

    Added HMR Code to Index

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement,
+  );
+}
+
+if (rootElement) {
+  if (module.hot) {
+    module.hot.accept('./App', () => setTimeout(render));
+  }
+
+  render();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
