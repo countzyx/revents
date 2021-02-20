2e67fba292f7508dc101c73c44cd4ba3debb37ba
Pedro R. Jiménez <countzyx@gmail.com>
2021-02-20 15:11:56 -0500

    Added Event Types

---
+++ src/Shared/Types.ts
@@ -0,0 +1,20 @@
+export type EventAttendee = {
+  id: string;
+  name: string;
+  photoUrl: string; // should be URL?
+};
+
+export type EventCategory = 'culture' | 'drinks';
+
+export type Event = {
+  id: string;
+  title: string;
+  date: string; // a string for now, needs to be Date maybe?
+  category: EventCategory;
+  description: string;
+  city: string;
+  venue: string;
+  hostedBy: string;
+  hostPhotoUrl: string; // should be URL?
+  attendees?: EventAttendee[];
+};
