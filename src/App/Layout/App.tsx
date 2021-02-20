6d637d3a6baaa97e78640c31b1061aca4185c1fb
Pedro R. Jiménez <countzyx@gmail.com>
2021-02-20 05:41:54 -0500

    Added EventDashboard

--- src/App/Layout/App.tsx
+++ src/App/Layout/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import styles from './App.module.css';
+import EventDashboard from '../../Features/Events/EventDashboard/EventDashboard';
 
 const App: React.FC = () => {
   return (
     <div className={styles.App}>
       <h1>Re-vents</h1>
+      <EventDashboard />
     </div>
   );
 };
---
+++ src/Features/Events/EventDashboard/EventDashboard.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Grid } from 'semantic-ui-react';
+
+const EventDashboard: React.FC = () => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <h2>Left Column</h2>
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <h2>Right Column</h2>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default EventDashboard;
