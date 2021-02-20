314e3c96934c9bfeb759a1d96fd88a88a5fcf941
Pedro R. Jiménez <countzyx@gmail.com>
2021-02-20 15:12:06 -0500

    Added SampleData

---
+++ src/App/Api/SampleData.ts
@@ -0,0 +1,54 @@
+import type { Event } from '../../Shared/Types';
+
+export const SampleData: Event[] = [
+  {
+    id: '1',
+    title: 'Trip to Empire State building',
+    date: '2018-03-21',
+    category: 'culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'NY, USA',
+    venue: 'Empire State Building, 5th Avenue, New York, NY, USA',
+    hostedBy: 'Bob',
+    hostPhotoUrl: 'https://randomuser.me/api/portraits/men/20.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoUrl: 'https://randomuser.me/api/portraits/men/20.jpg',
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoUrl: 'https://randomuser.me/api/portraits/men/22.jpg',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: 'Trip to Punch and Judy Pub',
+    date: '2018-03-18',
+    category: 'drinks',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: 'Punch & Judy, Henrietta Street, London, UK',
+    hostedBy: 'Tom',
+    hostPhotoUrl: 'https://randomuser.me/api/portraits/men/22.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoUrl: 'https://randomuser.me/api/portraits/men/20.jpg',
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoUrl: 'https://randomuser.me/api/portraits/men/22.jpg',
+      },
+    ],
+  },
+];
+
+export default SampleData;
