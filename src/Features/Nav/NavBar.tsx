62a26d3c73262a6a458aed702c4ef38a7e1dd237
Pedro R. Jiménez <countzyx@gmail.com>
2021-02-25 16:09:15 -0500

    Added NavLink to NavBar to Link to Routes

--- src/Features/Nav/NavBar.tsx
+++ src/Features/Nav/NavBar.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
 
 const NavBar: React.FC = () => {
   return (
     <Menu inverted fixed='top'>
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} to='/' exact header>
           <img src='/assets/logo.png' alt='logo' style={{ marginRight: '15px' }} />
           Re-vents
         </Menu.Item>
-        <Menu.Item name='Events' />
-        <Menu.Item>
+        <Menu.Item as={NavLink} to='/events' name='Events' />
+        <Menu.Item as={NavLink} to='/createEvent'>
           <Button positive inverted content='Create Event' />
         </Menu.Item>
         <Menu.Item position='right'>
