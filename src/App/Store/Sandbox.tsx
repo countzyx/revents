f2d441c5bf574f2c94de88f406f11a62397a7c15
Pedro R. Jiménez <countzyx@gmail.com>
2021-03-08 22:37:41 -0500

    Wired Up the Sandbox Component and Redux

--- src/App/Store/Sandbox.tsx
+++ src/App/Store/Sandbox.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import { useSelector } from 'react-redux';
-import type { TestState } from '../../Features/Sandbox/testReducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'semantic-ui-react';
+import { DECREMENT_DATA, INCREMENT_DATA, TestState } from '../../Features/Sandbox/testReducer';
 
 const Sandbox: React.FC = () => {
   const data = useSelector((state: TestState) => state.data);
+  const dispatch = useDispatch();
+
   return (
     <>
       <h1>Testing 1, 2, 3...</h1>
       <h3>The data is: {data}</h3>
+      <Button content='Increment' color='green' onClick={() => dispatch({ type: INCREMENT_DATA })} />
+      <Button content='Decrement' color='red' onClick={() => dispatch({ type: DECREMENT_DATA })} />
     </>
   );
 };
--- src/Features/Sandbox/testReducer.ts
+++ src/Features/Sandbox/testReducer.ts
@@ -1,3 +1,8 @@
+import * as _ from 'lodash';
+import { Action } from '@reduxjs/toolkit';
+
+export const DECREMENT_DATA = 'DECREMENT_DATA';
+export const INCREMENT_DATA = 'INCREMENT_DATA';
 export type TestState = {
   data: number;
 };
@@ -6,8 +11,24 @@ const initialState: TestState = {
   data: 42,
 };
 
-const reducer = (state: TestState = initialState): TestState => {
-  return state;
+const reducer = (state: TestState = initialState, action: Action): TestState => {
+  const newState = _.cloneDeep(state);
+
+  switch (action.type) {
+    case DECREMENT_DATA: {
+      newState.data -= 1;
+      break;
+    }
+    case INCREMENT_DATA: {
+      newState.data += 1;
+      break;
+    }
+    default: {
+      return state;
+    }
+  }
+
+  return newState;
 };
 
 export default reducer;
