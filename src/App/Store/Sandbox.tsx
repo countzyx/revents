a760314830c43ba924fb5a9942b385f028cc0440
Pedro R. Jiménez <countzyx@gmail.com>
2021-03-08 19:31:53 -0500

    Sandbox Pulls Data with Selector

--- src/App/Store/Sandbox.tsx
+++ src/App/Store/Sandbox.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import { useSelector } from 'react-redux';
+import type { TestState } from '../../Features/Sandbox/testReducer';
 
 const Sandbox: React.FC = () => {
+  const data = useSelector((state: TestState) => state.data);
   return (
     <>
       <h1>Testing 1, 2, 3...</h1>
-      <h3>The data is: </h3>
+      <h3>The data is: {data}</h3>
     </>
   );
 };
