951a6cd863e3fde7f93c807bd8db6b9d2a078aa4
Pedro R. Jiménez <countzyx@gmail.com>
2021-03-14 05:56:24 -0400

    Wired Up Yup Validation to Formik on EventForm

--- src/Features/Events/Form/EventForm.tsx
+++ src/Features/Events/Form/EventForm.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import * as React from 'react';
 import _ from 'lodash';
-import { Button, FormField, Header, Segment } from 'semantic-ui-react';
+import { Button, FormField, Header, Label, Segment } from 'semantic-ui-react';
 import { useHistory, useParams, withRouter } from 'react-router-dom';
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
 import type { EventInfo } from '../../../App/Shared/Types';
 import { useAppDispatch, useAppSelector } from '../../../App/Store/hooks';
 import { createEvent, updateEvent } from '../eventsSlice';
@@ -25,6 +26,15 @@ const defaultValues: EventFormValues = {
   venue: '',
   date: '',
 };
+
+const validationSchema: Yup.SchemaOf<EventFormValues> = Yup.object({
+  title: Yup.string().required(),
+  category: Yup.string().required(),
+  description: Yup.string().required(),
+  city: Yup.string().required(),
+  venue: Yup.string().required(),
+  date: Yup.string().required(),
+});
 
 const blankEvent: EventInfo = {
   id: '',
@@ -49,7 +59,7 @@ const EventForm: React.FC = () => {
   const dispatch = useAppDispatch();
   const events = useAppSelector((state) => state.events.events);
   const selectedEvent = eventId ? events.find((e) => e.id === eventId) : undefined;
-  const initialValues: EventFormValues = selectedEvent ? { ...selectedEvent } : defaultValues;
+  const initialValues: EventFormValues = selectedEvent ?? defaultValues;
 
   const onFormSubmitHandler = (formValues: EventFormValues) => {
     if (selectedEvent) {
@@ -82,6 +92,7 @@ const EventForm: React.FC = () => {
       <Header content={selectedEvent ? 'Edit event' : 'Create a new event'} />
       <Formik
         initialValues={initialValues}
+        validationSchema={validationSchema}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             onFormSubmitHandler(values);
@@ -92,21 +103,39 @@ const EventForm: React.FC = () => {
         <Form className='ui form'>
           <FormField>
             <Field type='text' name='title' placeholder='Event title' />
+            <ErrorMessage name='title'>
+              {(error) => <Label basic color='red' content={error} />}
+            </ErrorMessage>
           </FormField>
           <FormField>
             <Field type='text' name='category' placeholder='Category' />
+            <ErrorMessage name='category'>
+              {(error) => <Label basic color='red' content={error} />}
+            </ErrorMessage>
           </FormField>
           <FormField>
             <Field type='text' name='description' placeholder='Description' />
+            <ErrorMessage name='description'>
+              {(error) => <Label basic color='red' content={error} />}
+            </ErrorMessage>
           </FormField>
           <FormField>
             <Field type='text' name='city' placeholder='City' />
+            <ErrorMessage name='city'>
+              {(error) => <Label basic color='red' content={error} />}
+            </ErrorMessage>
           </FormField>
           <FormField>
             <Field type='text' name='venue' placeholder='Venue' />
+            <ErrorMessage name='venue'>
+              {(error) => <Label basic color='red' content={error} />}
+            </ErrorMessage>
           </FormField>
           <FormField>
             <Field type='date' name='date' placeholder='Date' />
+            <ErrorMessage name='date'>
+              {(error) => <Label basic color='red' content={error} />}
+            </ErrorMessage>
           </FormField>
           <Button type='submit' floated='right' positive content='Submit' />
           <Button floated='right' content='Cancel' onClick={onCancel} />
