bbf3bdfa1f8765610887b5fefe74623719056513
Pedro R. Jiménez <countzyx@gmail.com>
2021-04-15 05:27:47 -0400

    Switch to React-Places-Autocomplete API
    Switching from react-google-places-autocomplete to
    react-places-autocomplete.
    Though the first component seems better and does not need the script tag
    in the HTML to load the Places library, the second is what is used in
    the lesson and is easier to make a component with since it is more
    amenable to styling.
    
    Added script tag to HTML to load Places API. The key reads from an
    environment variable, in our case from an ENV file not in Git.

--- src/Features/Sandbox/TestGooglePlaces.tsx
+++ src/Features/Sandbox/TestGooglePlaces.tsx
@@ -1,13 +1,58 @@
+/* eslint-disable react/jsx-props-no-spreading */
+// This API relies on spreading helper function outputs; this linter rule makes the code less maintainable.
 import * as React from 'react';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
-import { GoogleMapsApiKey } from '../../App/Shared/Constants';
+import PlacesAutoComplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 const TestGooglePlaces: React.FC = () => {
-  if (!GoogleMapsApiKey) {
-    return <div>No Google API Key found</div>;
-  }
+  const [addressState, setAddressState] = React.useState('');
+  const handleChange = (address: string) => {
+    setAddressState(address);
+  };
 
-  return <GooglePlacesAutocomplete apiKey={GoogleMapsApiKey} />;
+  const handleSelect = (address: string, _: string) => {
+    geocodeByAddress(address)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => console.log('Success', latLng))
+      .catch((error) => console.error('Error', error));
+    setAddressState(address);
+  };
+
+  return (
+    <PlacesAutoComplete value={addressState} onChange={handleChange} onSelect={handleSelect}>
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div>
+          <input
+            {...getInputProps({
+              placeholder: 'Search Places ...',
+              className: 'location-search-input',
+            })}
+          />
+          <div className='autocomplete-dropdown-container'>
+            {loading && <div>Loading...</div>}
+            {suggestions.map((suggestion) => {
+              const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                : { backgroundColor: '#ffffff', cursor: 'pointer' };
+              return (
+                // The key is actually return in the spread, but the linter can't figure that out, so we have to disable jsx-key
+                // eslint-disable-next-line react/jsx-key
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style,
+                  })}
+                >
+                  <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </PlacesAutoComplete>
+  );
 };
 
 export default TestGooglePlaces;
