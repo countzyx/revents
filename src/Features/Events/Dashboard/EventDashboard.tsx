60e7d442b10225273b3914c97a3be472f1aa6394
Pedro R. Jiménez <countzyx@gmail.com>
2021-04-28 08:32:27 -0400

    Added EventFilters to EventDashboard

--- src/Features/Events/Dashboard/EventDashboard.tsx
+++ src/Features/Events/Dashboard/EventDashboard.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Grid } from 'semantic-ui-react';
 import { useAppDispatch, useAppSelector } from '../../../App/Store/hooks';
 import { fetchEvents, selectEventsIsLoading } from '../eventsSlice';
+import EventFilters from './EventFilters';
 import EventListItemPlaceholder from './EventListItemPlaceholder';
 import EventList from './EventsList';
 
@@ -25,7 +26,7 @@ const EventDashboard: React.FC = () => {
         <EventList />
       </Grid.Column>
       <Grid.Column width={6}>
-        <h1>Event Filters</h1>
+        <EventFilters />
       </Grid.Column>
     </Grid>
   );
