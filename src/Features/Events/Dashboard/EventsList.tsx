7508341284dff1790dbbb7040bd9d2b864e8beb5
Pedro R. Jiménez <countzyx@gmail.com>
2021-04-28 06:29:11 -0400

    Wired up Events to Use MockApi for Events

--- src/Features/Events/Dashboard/EventsList.tsx
+++ src/Features/Events/Dashboard/EventsList.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import EventListItem from './EventListItem';
-import { useAppSelector } from '../../../App/Store/hooks';
-import { selectEvents } from '../eventsSlice';
+import { useAppDispatch, useAppSelector } from '../../../App/Store/hooks';
+import { fetchEvents, selectEvents } from '../eventsSlice';
 
 const EventList: React.FC = () => {
   const events = useAppSelector(selectEvents);
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchEvents());
+  }, [dispatch]);
 
   return (
     <>
--- src/Features/Events/eventsSlice.ts
+++ src/Features/Events/eventsSlice.ts
@@ -1,30 +1,60 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { EventInfo } from '../../App/Shared/Types';
-import SampleData from '../../App/Api/SampleData';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import fetchSampleData from '../../App/Api/MockApi';
+import type { AsyncState, EventInfo } from '../../App/Shared/Types';
 import { RootState } from '../../App/Store/store';
 
 type EventState = {
   events: EventInfo[];
-};
+} & AsyncState;
 
 const initialState: EventState = {
-  events: SampleData,
+  error: undefined,
+  events: [],
+  isLoading: false,
 };
+
+export const fetchEvents = createAsyncThunk('events/fetchEvents', async (thunkApi) =>
+  fetchSampleData(),
+);
 
 export const eventsSlice = createSlice({
   name: 'events',
   initialState,
   reducers: {
     createEvent: (state, action: PayloadAction<EventInfo>) => ({
+      ...state,
       events: [...state.events, action.payload],
     }),
     deleteEvent: (state, action: PayloadAction<string>) => ({
+      ...state,
       events: state.events.filter((e) => e.id !== action.payload),
     }),
     updateEvent: (state, action: PayloadAction<EventInfo>) => {
       const updatedEvent = action.payload;
-      return { events: state.events.map((e) => (e.id === updatedEvent.id ? updatedEvent : e)) };
+      return {
+        ...state,
+        events: state.events.map((e) => (e.id === updatedEvent.id ? updatedEvent : e)),
+      };
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchEvents.fulfilled, (state, action) => ({
+        ...state,
+        error: undefined,
+        events: action.payload,
+        isLoading: false,
+      }))
+      .addCase(fetchEvents.pending, (state) => ({
+        ...state,
+        error: undefined,
+        isLoading: true,
+      }))
+      .addCase(fetchEvents.rejected, (state, action) => ({
+        ...state,
+        error: action.error as Error,
+        isLoading: false,
+      }));
   },
 });
 
