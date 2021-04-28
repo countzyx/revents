abfe8d7ed5c9c41a9c5a5d9b4adb7393d0ea28f2
Pedro R. Jiménez <countzyx@gmail.com>
2021-04-28 07:16:47 -0400

    Replaced LoadingComponent with Placeholders in Events

--- src/Features/Events/Dashboard/EventDashboard.tsx
+++ src/Features/Events/Dashboard/EventDashboard.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Grid } from 'semantic-ui-react';
-import LoadingComponent from '../../../App/Layout/LoadingComponent';
 import { useAppDispatch, useAppSelector } from '../../../App/Store/hooks';
 import { fetchEvents, selectEventsIsLoading } from '../eventsSlice';
+import EventListItemPlaceholder from './EventListItemPlaceholder';
 import EventList from './EventsList';
 
 const EventDashboard: React.FC = () => {
@@ -13,13 +13,15 @@ const EventDashboard: React.FC = () => {
     dispatch(fetchEvents());
   }, [dispatch]);
 
-  if (isLoading) {
-    return <LoadingComponent />;
-  }
-
   return (
     <Grid>
       <Grid.Column width={10}>
+        {isLoading && (
+          <>
+            <EventListItemPlaceholder />
+            <EventListItemPlaceholder />
+          </>
+        )}
         <EventList />
       </Grid.Column>
       <Grid.Column width={6}>
