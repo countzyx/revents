1be4cce04bdf2ed14dd2c9e4198f836563565c67
Pedro R. Jiménez <countzyx@gmail.com>
2021-09-29 20:20:47 -0400

    Accomodated Undefined in User Profile
    Might use the date converter for the createAt timestamp.

--- src/App/Firebase/FirestoreUserProfileService.ts
+++ src/App/Firebase/FirestoreUserProfileService.ts
@@ -1,16 +1,21 @@
 import type { User } from 'firebase/auth';
 import { collection, doc, serverTimestamp, setDoc } from 'firebase/firestore';
+// import { UserProfile } from '../Shared/Types';
 import { db } from './Firebase';
+// import dateConverter from './FirestoreUtil';
 
 const userProfileCollection = collection(db, 'users');
 
 export const setUserProfileInFirestore = async (user: User): Promise<void> => {
   const { displayName, email, photoURL } = user;
+  if (!email) {
+    throw new Error('email is null');
+  }
   return setDoc(doc(userProfileCollection, user.uid), {
     createdAt: serverTimestamp(),
-    displayName,
+    displayName: displayName || undefined,
     email,
-    photoURL,
+    photoURL: photoURL || undefined,
   });
 };
 
