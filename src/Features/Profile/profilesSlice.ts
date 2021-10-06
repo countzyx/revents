12bb85472e6f69089b810b89222fe6ae36f2b3f6
Pedro R. Jiménez <countzyx@gmail.com>
2021-10-06 06:10:15 -0400

    Added FetchUserProfile Thunk to Profiles Slice

--- src/Features/Profile/profilesSlice.ts
+++ src/Features/Profile/profilesSlice.ts
@@ -1,15 +1,39 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  getUserProfileFromFirestore,
+  Unsubscribe,
+} from '../../App/Firebase/FirestoreUserProfileService';
 import { UserProfile } from '../../App/Shared/Types';
-import { RootState } from '../../App/Store/store';
+import { AppDispatch, RootState } from '../../App/Store/store';
 
 type ProfileState = {
   currentProfile?: UserProfile;
   error?: Error;
+  isLoading: boolean;
 };
 
 const initialState: ProfileState = {
   currentProfile: undefined,
   error: undefined,
+  isLoading: false,
+};
+
+export const fetchUserProfile = (dispatch: AppDispatch, eventId: string): Unsubscribe => {
+  const { fetchUserProfilePending, fetchUserProfileFulfilled, fetchUserProfileRejected } =
+    profilesSlice.actions;
+  const unsubscribe = getUserProfileFromFirestore(
+    {
+      next: async (snapshot) => {
+        dispatch(fetchUserProfilePending());
+        const fetchedUserProfile = snapshot.data() as UserProfile;
+        dispatch(fetchUserProfileFulfilled(fetchedUserProfile));
+      },
+      error: async (err) => dispatch(fetchUserProfileRejected(err)),
+    },
+    eventId,
+  );
+
+  return unsubscribe;
 };
 
 export const profilesSlice = createSlice({
@@ -20,6 +44,24 @@ export const profilesSlice = createSlice({
       ...state,
       error: undefined,
     }),
+    fetchUserProfilePending: (state) => ({
+      ...state,
+      currentProfile: undefined,
+      error: undefined,
+      isLoading: true,
+    }),
+    fetchUserProfileFulfilled: (state, action: PayloadAction<UserProfile>) => ({
+      ...state,
+      currentProfile: action.payload,
+      error: undefined,
+      isLoading: false,
+    }),
+    fetchUserProfileRejected: (state, action: PayloadAction<Error>) => ({
+      ...state,
+      currentProfile: undefined,
+      error: action.payload,
+      isLoading: false,
+    }),
     setError: (state, action: PayloadAction<Error>) => ({
       ...state,
       error: action.payload,
@@ -28,6 +70,9 @@ export const profilesSlice = createSlice({
 });
 
 export const { clearError } = profilesSlice.actions;
+export const selectProfileCurrentProfile = (state: RootState): UserProfile | undefined =>
+  state.profiles.currentProfile;
 export const selectProfileError = (state: RootState): Error | undefined => state.profiles.error;
+export const selectProfileIsLoading = (state: RootState): boolean => state.profiles.isLoading;
 
 export default profilesSlice.reducer;
