25aa01b834b83c8bf09d1ce19a3a84a09081e481
Pedro R. Jiménez <countzyx@gmail.com>
2021-10-27 05:36:05 -0400

    Added Form Submit to Profile Form

--- src/Features/Profile/ProfilePage/ProfileForm.tsx
+++ src/Features/Profile/ProfilePage/ProfileForm.tsx
@@ -1,8 +1,11 @@
 import { Form, Formik } from 'formik';
 import * as React from 'react';
-import { Button, FormTextArea } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 import FormTextInput from '../../../App/Components/Form/FormTextInput';
+import FormTextArea from '../../../App/Components/Form/FormTextArea';
+import { updateUserProfileInFirestore } from '../../../App/Firebase/FirestoreUserProfileService';
 import { useAppSelector } from '../../../App/Store/hooks';
 import { selectProfileCurrentProfile } from '../profilesSlice';
 
@@ -22,18 +25,39 @@ const ProfileForm: React.FC = () => {
   if (!currentProfile) return <div />;
 
   const { description, displayName } = currentProfile;
-  const initialValues: ProfileFormValues = { displayName: displayName || '', description };
+  const initialValues: ProfileFormValues = {
+    displayName: displayName || '',
+    description: description || '',
+  };
 
+  const onFormSubmitHandler = async (
+    formValues: ProfileFormValues,
+    setSubmitting: (isSubmitting: boolean) => void,
+  ) => {
+    try {
+      await updateUserProfileInFirestore({ ...currentProfile, ...formValues });
+    } catch (anyErr) {
+      const err = anyErr as Error;
+      if (err) {
+        toast.error(err.message);
+      } else {
+        toast.error(String(anyErr));
+      }
+      setSubmitting(false);
+    }
+  };
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={async (values, { setSubmitting }) => {
+        await onFormSubmitHandler(values, setSubmitting);
+      }}
     >
       {(formik) => (
         <Form className='ui form'>
-          <FormTextInput name='displayName' placeholder='Display Name' />
-          <FormTextArea name='description' placeholder='Description' />
+          <FormTextInput name='displayName' placeholder='Display Name' type='text' />
+          <FormTextArea name='description' placeholder='Description' rows={3} type='text' />
           <Button
             content='Update profile'
             disabled={!(formik.isValid && formik.dirty && !formik.isSubmitting)}
