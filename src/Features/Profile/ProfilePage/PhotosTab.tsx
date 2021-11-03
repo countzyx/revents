54a8864e269e44b1e7c28ed25d93f69ede0f1a0e
Pedro R. Jiménez <countzyx@gmail.com>
2021-11-03 08:20:14 -0400

    Started Photos Tab for Profile Page

---
+++ src/Features/Profile/ProfilePage/PhotosTab.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Button, Card, Grid, Header, Image, Tab } from 'semantic-ui-react';
+import { useAppSelector } from '../../../App/Store/hooks';
+import { selectAuthUserInfo } from '../../Auth/authSlice';
+import { selectProfileSelectedProfile } from '../profilesSlice';
+
+const PhotosTab: React.FC = () => {
+  const [isEditable, setIsEditable] = React.useState(false);
+  const currentUser = useAppSelector(selectAuthUserInfo);
+  const selectedProfile = useAppSelector(selectProfileSelectedProfile);
+  const isCurrentUser = currentUser && currentUser.uid === selectedProfile?.id;
+
+  if (!selectedProfile) return <div />;
+
+  const { displayName } = selectedProfile;
+
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header content={`${displayName} Photos`} floated='left' icon='images' />
+          {isCurrentUser && (
+            <Button
+              basic
+              content={isEditable ? 'Cancel' : 'Add Photo'}
+              onClick={() => setIsEditable(!isEditable)}
+              floated='right'
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {isEditable ? (
+            <p>Photo widgets</p>
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              <Card>
+                <Image src='/assets/user.png' />
+                <Button.Group fluid widths={2}>
+                  <Button basic color='green' content='Main' />
+                  <Button basic color='red' icon='trash' />
+                </Button.Group>
+              </Card>
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+};
+
+export default PhotosTab;
--- src/Features/Profile/ProfilePage/ProfileContent.tsx
+++ src/Features/Profile/ProfilePage/ProfileContent.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Tab } from 'semantic-ui-react';
 import AboutTab from './AboutTab';
+import PhotosTab from './PhotosTab';
 
 const ProfileContent: React.FC = () => {
   const tabPanes = [
     { menuItem: 'About', render: () => <AboutTab /> },
-    { menuItem: 'Photos', render: () => <Tab.Pane>Photos</Tab.Pane> },
+    { menuItem: 'Photos', render: () => <PhotosTab /> },
     { menuItem: 'Events', render: () => <Tab.Pane>Events</Tab.Pane> },
     { menuItem: 'Followers', render: () => <Tab.Pane>Followers</Tab.Pane> },
     { menuItem: 'Following', render: () => <Tab.Pane>Following</Tab.Pane> },
