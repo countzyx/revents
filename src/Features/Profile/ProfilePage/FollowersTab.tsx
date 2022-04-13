5e0944ebfb3a553339c9d9d4a7323fbaca556e23
Pedro R. Jiménez <countzyx@gmail.com>
2022-04-13 05:10:16 -0400

    Fleshing out FollowersTab.

--- src/Features/Profile/ProfilePage/FollowersTab.tsx
+++ src/Features/Profile/ProfilePage/FollowersTab.tsx
@@ -1,5 +1,41 @@
 import * as React from 'react';
+import { Card, Grid, Header, Tab } from 'semantic-ui-react';
+import { kUnknownUserImageUrl } from '../../../App/Shared/Constants';
+import { UserBasicInfo } from '../../../App/Shared/Types';
+import { useAppSelector } from '../../../App/Store/hooks';
+// import { selectAuthUserInfo } from '../../Auth/authSlice';
+import { selectProfileSelectedProfile } from '../profilesSlice';
+import ProfileCard from './ProfileCard';
 
-const FollowersTab = () => <div />;
+const FollowersTab: React.FC = () => {
+  //  const currentUser = useAppSelector(selectAuthUserInfo);
+  const selectedProfile = useAppSelector(selectProfileSelectedProfile);
+  //  const isCurrentUser = currentUser && currentUser.uid === selectedProfile?.id;
+
+  if (!selectedProfile || !selectedProfile.displayName) return <div />;
+
+  // reusing profile to check the layout. will replace with follower data from Firestore.
+  const profileInfo: UserBasicInfo = {
+    displayName: selectedProfile.displayName,
+    id: selectedProfile.id,
+    photoURL: selectedProfile.photoURL || kUnknownUserImageUrl,
+  };
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header content='Followers' floated='left' icon='user' />
+        </Grid.Column>
+        <Grid.Column width={16}>
+          <Card.Group itemsPerRow={5}>
+            <ProfileCard profileInfo={profileInfo} />
+            <ProfileCard profileInfo={profileInfo} />
+            <ProfileCard profileInfo={profileInfo} />
+          </Card.Group>
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+};
 
 export default FollowersTab;
