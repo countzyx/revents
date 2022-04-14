48a4484262d7b7cf78607fdf54b6c6f427479c1e
Pedro R. Jiménez <countzyx@gmail.com>
2022-04-14 02:27:32 -0400

    FollowingTab Loads Following from Firestore
    For the selected profile.

--- src/Features/Profile/ProfilePage/FollowingTab.tsx
+++ src/Features/Profile/ProfilePage/FollowingTab.tsx
@@ -1,5 +1,46 @@
 import * as React from 'react';
+import { Card, Grid, Header, Tab } from 'semantic-ui-react';
+import { useAppDispatch, useAppSelector } from '../../../App/Store/hooks';
+import {
+  fetchFollowingForProfile,
+  selectProfileFollowing,
+  selectProfileIsLoadingFollowing,
+  selectProfileSelectedProfile,
+} from '../profilesSlice';
+import ProfileCard from './ProfileCard';
 
-const FollowingTab = () => <div />;
+const FollowingTab: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const selectedProfile = useAppSelector(selectProfileSelectedProfile);
+  const following = useAppSelector(selectProfileFollowing);
+  const isLoadingFollowing = useAppSelector(selectProfileIsLoadingFollowing);
+
+  React.useEffect(() => {
+    if (!selectedProfile) return undefined;
+
+    const unsubscribe = fetchFollowingForProfile(dispatch, selectedProfile.id);
+    return unsubscribe;
+  }, [dispatch, selectedProfile]);
+
+  if (!selectedProfile || !selectedProfile.displayName) return <div />;
+
+  return (
+    <Tab.Pane loading={isLoadingFollowing}>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header content='Following' floated='left' icon='heart' />
+        </Grid.Column>
+        <Grid.Column width={16}>
+          <Card.Group itemsPerRow={5}>
+            {following.length > 0 &&
+              following.map((followingInfo) => (
+                <ProfileCard key={followingInfo.id} profileInfo={followingInfo} />
+              ))}
+          </Card.Group>
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+};
 
 export default FollowingTab;
