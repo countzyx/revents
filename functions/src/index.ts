9590a3cfe5c90f2b54969cf19db025987293f405
Pedro R. Jiménez <countzyx@gmail.com>
2022-04-26 08:30:04 -0400

    Added Cloud Functions for Relationships
    With the new security settings, users cannot change other users' data.
    So Cloud Functions have to make changes on other profiles when the user
    follows or unfollows other users.
    CreateFollowerOnCreateFollowing creates a doc of the user as a follower
    in the relationship doc of the target of their follow that triggers when the
    user creates a doc in the relationships of the user they are following.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,52 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const kRelationshipsFollowingPath = 'relationships/{userId}/following/{profileId}';
+const relationshipsCollection = db.collection('relationships');
+const userCollection = db.collection('users');
+
+export const createFollowerOnCreateFollowing = functions.firestore
+  .document(kRelationshipsFollowingPath)
+  .onCreate(async (snap, context) => {
+    const following = snap.data();
+    console.log(following);
+    try {
+      const { profileId, userId } = context.params;
+      const userDoc = await userCollection.doc(userId).get();
+      if (!userDoc.exists) return console.log(`User ${userId} does not exist.`);
+      const userInfo = userDoc.data();
+      if (!userInfo) return console.log(`User ${userId} is undefined.`);
+      const { displayName, photoURL } = userInfo;
+      const batch = db.batch();
+      batch.set(relationshipsCollection.doc(profileId).collection('followers').doc(userId), {
+        id: userDoc.id,
+        displayName,
+        photoURL,
+      });
+      batch.update(userCollection.doc(profileId), {
+        followerCount: admin.firestore.FieldValue.increment(1),
+      });
+      return await batch.commit();
+    } catch (err) {
+      return console.log(err);
+    }
+  });
+
+export const deleteFollowerOnDeleteFollowing = functions.firestore
+  .document(kRelationshipsFollowingPath)
+  .onDelete(async (snap, context) => {
+    try {
+      const batch = db.batch();
+      const { profileId, userId } = context.params;
+      batch.delete(relationshipsCollection.doc(profileId).collection('followers').doc(userId));
+      batch.update(userCollection.doc(profileId), {
+        followerCount: admin.firestore.FieldValue.increment(-1),
+      });
+
+      return await batch.commit();
+    } catch (err) {
+      return console.log(err);
+    }
+  });
