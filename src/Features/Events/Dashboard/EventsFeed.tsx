6ee9679f6af30359ed74d7226360d8ad89804227
Pedro R. Jiménez <countzyx@gmail.com>
2022-05-02 17:35:01 -0400

    Mocked Up a UI for EventsFeed

--- src/Features/Events/Dashboard/EventsFeed.tsx
+++ src/Features/Events/Dashboard/EventsFeed.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
+import { Feed, Header, Segment } from 'semantic-ui-react';
+import { kUnknownUserImageUrl } from '../../../App/Shared/Constants';
 
-const EventsFeed: React.FC = () => <div />;
+const EventsFeed: React.FC = () => {
+  const date = '3 days ago';
+  const summary = 'Diana joined an event';
+
+  return (
+    <>
+      <Header attached color='teal' content='News Feed' icon='newspaper' />
+      <Segment attached='bottom'>
+        <Feed>
+          <Feed.Event date={date} image={kUnknownUserImageUrl} summary={summary} />
+          <Feed.Event date={date} image={kUnknownUserImageUrl} summary={summary} />
+          <Feed.Event date={date} image={kUnknownUserImageUrl} summary={summary} />
+        </Feed>
+      </Segment>
+    </>
+  );
+};
 
 export default EventsFeed;
