81a2a6503c78647ac4926871f8f16a1cb1ca6633
Pedro R. Jiménez <countzyx@gmail.com>
2022-05-13 06:26:29 -0400

    Hiding Event Chat for Anonymous

--- src/Features/Events/Details/EventDetailsChat.tsx
+++ src/Features/Events/Details/EventDetailsChat.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { Comment, Header, Segment } from 'semantic-ui-react';
+import { selectAuthIsAuthed } from 'src/Features/Auth/authSlice';
 import { kUnknownUserImageUrl } from '../../../App/Shared/Constants';
 import { useAppDispatch, useAppSelector } from '../../../App/Store/hooks';
 import {
@@ -22,6 +23,7 @@ const EventDetailsChat: React.FC<Props> = (props) => {
   const dispatch = useAppDispatch();
   const chatComments = useAppSelector(selectEventsChatComments);
   const chatError = useAppSelector(selectEventsChatError);
+  const isAuthed = useAppSelector(selectAuthIsAuthed);
   const isLoadingChat = useAppSelector(selectEventsIsLoadingChat);
   const [replyFormTarget, setReplyFormTarget] = React.useState<string | undefined>(undefined);
 
@@ -49,91 +51,93 @@ const EventDetailsChat: React.FC<Props> = (props) => {
   return (
     <>
       <Segment textAlign='center' attached='top' inverted color='teal' style={{ border: 'none' }}>
-        <Header>Chat about this event</Header>
+        <Header>{isAuthed ? 'Chat about this event' : 'Sign in to view and comment'}</Header>
       </Segment>
 
-      <Segment attached loading={isLoadingChat}>
-        <EventDetailsChatForm eventId={eventId} />
-        <Comment.Group>
-          {chatComments.map(
-            (c) =>
-              c.id && ( // c.id is not undefined or null
-                <Comment key={c.id}>
-                  <Comment.Avatar src={c.photoURL || kUnknownUserImageUrl} />
-                  <Comment.Content>
-                    <Comment.Author as={Link} to={`/profile/${c.uid}`}>
-                      {c.name}
-                    </Comment.Author>
-                    <Comment.Metadata>
-                      <div>{c.datetime}</div>
-                    </Comment.Metadata>
-                    <Comment.Text>
-                      {c.text.split('\n').map((s, i) => (
-                        // disabling eslint check because we are just breaking down string in to an array of chunks
-                        // eslint-disable-next-line react/no-array-index-key
-                        <span key={`${c.id}-${i}`}>
-                          {s}
-                          <br />
-                        </span>
-                      ))}
-                    </Comment.Text>
-                    <Comment.Actions>
-                      <Comment.Action onClick={() => handleOpenReplyForm(c.id)}>
-                        Reply
-                      </Comment.Action>
-                      {replyFormTarget === c.id && (
-                        <EventDetailsChatForm
-                          eventId={eventId}
-                          onClose={handleCloseReplyForm}
-                          parentCommentId={c.id}
-                        />
-                      )}
-                    </Comment.Actions>
-                  </Comment.Content>
-                  {c.children && c.children.length > 0 && (
-                    <Comment.Group>
-                      {c.children.map((child) => (
-                        <Comment key={child.id}>
-                          <Comment.Avatar src={child.photoURL || kUnknownUserImageUrl} />
-                          <Comment.Content>
-                            <Comment.Author as={Link} to={`/profile/${child.uid}`}>
-                              {child.name}
-                            </Comment.Author>
-                            <Comment.Metadata>
-                              <div>{child.datetime}</div>
-                            </Comment.Metadata>
-                            <Comment.Text>
-                              {child.text.split('\n').map((s, i) => (
-                                // disabling eslint check because we are just breaking down string in to an array of chunks
-                                // eslint-disable-next-line react/no-array-index-key
-                                <span key={`${child.id}-${i}`}>
-                                  {s}
-                                  <br />
-                                </span>
-                              ))}
-                            </Comment.Text>
-                            <Comment.Actions>
-                              <Comment.Action onClick={() => handleOpenReplyForm(child.id)}>
-                                Reply
-                              </Comment.Action>
-                              {replyFormTarget === child.id && (
-                                <EventDetailsChatForm
-                                  eventId={eventId}
-                                  onClose={handleCloseReplyForm}
-                                  parentCommentId={child.parentId}
-                                />
-                              )}
-                            </Comment.Actions>
-                          </Comment.Content>
-                        </Comment>
-                      ))}
-                    </Comment.Group>
-                  )}
-                </Comment>
-              ),
-          )}
-        </Comment.Group>
-      </Segment>
+      {isAuthed && (
+        <Segment attached loading={isLoadingChat}>
+          <EventDetailsChatForm eventId={eventId} />
+          <Comment.Group>
+            {chatComments.map(
+              (c) =>
+                c.id && ( // c.id is not undefined or null
+                  <Comment key={c.id}>
+                    <Comment.Avatar src={c.photoURL || kUnknownUserImageUrl} />
+                    <Comment.Content>
+                      <Comment.Author as={Link} to={`/profile/${c.uid}`}>
+                        {c.name}
+                      </Comment.Author>
+                      <Comment.Metadata>
+                        <div>{c.datetime}</div>
+                      </Comment.Metadata>
+                      <Comment.Text>
+                        {c.text.split('\n').map((s, i) => (
+                          // disabling eslint check because we are just breaking down string in to an array of chunks
+                          // eslint-disable-next-line react/no-array-index-key
+                          <span key={`${c.id}-${i}`}>
+                            {s}
+                            <br />
+                          </span>
+                        ))}
+                      </Comment.Text>
+                      <Comment.Actions>
+                        <Comment.Action onClick={() => handleOpenReplyForm(c.id)}>
+                          Reply
+                        </Comment.Action>
+                        {replyFormTarget === c.id && (
+                          <EventDetailsChatForm
+                            eventId={eventId}
+                            onClose={handleCloseReplyForm}
+                            parentCommentId={c.id}
+                          />
+                        )}
+                      </Comment.Actions>
+                    </Comment.Content>
+                    {c.children && c.children.length > 0 && (
+                      <Comment.Group>
+                        {c.children.map((child) => (
+                          <Comment key={child.id}>
+                            <Comment.Avatar src={child.photoURL || kUnknownUserImageUrl} />
+                            <Comment.Content>
+                              <Comment.Author as={Link} to={`/profile/${child.uid}`}>
+                                {child.name}
+                              </Comment.Author>
+                              <Comment.Metadata>
+                                <div>{child.datetime}</div>
+                              </Comment.Metadata>
+                              <Comment.Text>
+                                {child.text.split('\n').map((s, i) => (
+                                  // disabling eslint check because we are just breaking down string in to an array of chunks
+                                  // eslint-disable-next-line react/no-array-index-key
+                                  <span key={`${child.id}-${i}`}>
+                                    {s}
+                                    <br />
+                                  </span>
+                                ))}
+                              </Comment.Text>
+                              <Comment.Actions>
+                                <Comment.Action onClick={() => handleOpenReplyForm(child.id)}>
+                                  Reply
+                                </Comment.Action>
+                                {replyFormTarget === child.id && (
+                                  <EventDetailsChatForm
+                                    eventId={eventId}
+                                    onClose={handleCloseReplyForm}
+                                    parentCommentId={child.parentId}
+                                  />
+                                )}
+                              </Comment.Actions>
+                            </Comment.Content>
+                          </Comment>
+                        ))}
+                      </Comment.Group>
+                    )}
+                  </Comment>
+                ),
+            )}
+          </Comment.Group>
+        </Segment>
+      )}
     </>
   );
 };
