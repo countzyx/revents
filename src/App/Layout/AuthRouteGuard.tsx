af01b673a6a34f657cd78d0f3acbb3df8804d267
Pedro R. Jiménez <countzyx@gmail.com>
2022-08-04 00:39:57 -0400

    Updated ESLintRC to Allow Fragment Expresson

--- src/App/Layout/AuthRouteGuard.tsx
+++ src/App/Layout/AuthRouteGuard.tsx
@@ -18,8 +18,7 @@ const AuthRouteGuard: React.FC<Props> = (props) => {
     !isAuthed && dispatch(openModal({ modalType: 'UnauthModal' }));
   }, [isAuthed, dispatch]);
 
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{children}</>; // this fragment is not useless; it's an easy fix for the type
+  return isAuthed ? <>{children}</> : null;
 };
 
 AuthRouteGuard.defaultProps = {
