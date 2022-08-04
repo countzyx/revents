459b23810aa714adbe145a8e0502f449720becd5
Pedro R. Jiménez <countzyx@gmail.com>
2022-08-04 07:30:37 -0400

    Show Event Filters to Authed Users Only

--- src/Features/Events/Dashboard/EventFilters.tsx
+++ src/Features/Events/Dashboard/EventFilters.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Calendar from 'react-calendar';
 import { Header, Menu } from 'semantic-ui-react';
+import { selectAuthIsAuthed } from 'src/Features/Auth/authSlice';
 import type { CriteriaKeys, FilterValues, EventSearchCriteria } from '../../../App/Shared/Types';
 import { getDateTimeStringFromDate } from '../../../App/Shared/Utils';
 import { useAppDispatch, useAppSelector } from '../../../App/Store/hooks';
@@ -14,6 +15,7 @@ import {
 import styles from './EventFilters.module.css';
 
 const EventFilters: React.FC = () => {
+  const isAuthed = useAppSelector(selectAuthIsAuthed);
   const isLoadingEvents = useAppSelector(selectEventsIsLoading);
   const searchCriteria = useAppSelector(selectEventsSearchCriteria);
   const dispatch = useAppDispatch();
@@ -27,27 +29,29 @@ const EventFilters: React.FC = () => {
 
   return (
     <>
-      <Menu className={styles.FilterMenu} size='large' vertical>
-        <Header attached color='teal' content='Filters' icon='filter' />
-        <Menu.Item
-          active={searchCriteria.filter === 'all'}
-          content='All events'
-          disabled={isLoadingEvents}
-          onClick={() => onUpdateSearchCriteria('filter', 'all')}
-        />
-        <Menu.Item
-          active={searchCriteria.filter === 'isGoing'}
-          content="I'm going"
-          disabled={isLoadingEvents}
-          onClick={() => onUpdateSearchCriteria('filter', 'isGoing')}
-        />
-        <Menu.Item
-          active={searchCriteria.filter === 'isHost'}
-          content="I'm hosting"
-          disabled={isLoadingEvents}
-          onClick={() => onUpdateSearchCriteria('filter', 'isHost')}
-        />
-      </Menu>
+      {isAuthed && (
+        <Menu className={styles.FilterMenu} size='large' vertical>
+          <Header attached color='teal' content='Filters' icon='filter' />
+          <Menu.Item
+            active={searchCriteria.filter === 'all'}
+            content='All events'
+            disabled={isLoadingEvents}
+            onClick={() => onUpdateSearchCriteria('filter', 'all')}
+          />
+          <Menu.Item
+            active={searchCriteria.filter === 'isGoing'}
+            content="I'm going"
+            disabled={isLoadingEvents}
+            onClick={() => onUpdateSearchCriteria('filter', 'isGoing')}
+          />
+          <Menu.Item
+            active={searchCriteria.filter === 'isHost'}
+            content="I'm hosting"
+            disabled={isLoadingEvents}
+            onClick={() => onUpdateSearchCriteria('filter', 'isHost')}
+          />
+        </Menu>
+      )}
       <Header attached color='teal' content='Select date' icon='calendar' />
       <Calendar
         className={styles.Calendar}
